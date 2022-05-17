f34f4cc8ddcbf68c6fa208716e202f989b0fe3c3
Jackson Holliday <jaxholliday@gmail.com>
2022-05-17 11:11:07 -0400

    Add project files.

---
+++ TheBugTracker/Models/BTUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class BTUser : IdentityUser  //inherits identity user role/claim and log in 
+    {
+        // ability to customize a user 
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get { return $"{FirstName } {LastName}"; } }
+
+    }
+}
