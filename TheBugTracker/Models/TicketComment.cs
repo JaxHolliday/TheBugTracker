0d5e0b70bc01a19f9074986ed4eacc2b030908fd
Jackson Holliday <jaxholliday@gmail.com>
2022-05-18 16:37:09 -0400

    Comment model

---
+++ TheBugTracker/Models/TicketComment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        //Navigation Props - allows us to specify the relationship from one class to another 
+        public virtual Ticket Ticket { get; set; }  // Ticket table to a particular ticket 
+
+        public virtual BTUser User { get; set; }
+
+    }
+}
