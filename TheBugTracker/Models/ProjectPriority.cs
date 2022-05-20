b618e72c6aaf06508265a97482aef5370eca000a
Jackson Holliday <jaxholliday@gmail.com>
2022-05-20 10:22:07 -0400

    Project Model

---
+++ TheBugTracker/Models/ProjectPriority.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class ProjectPriority
+    {
+
+        //Primary Key
+        public int Id { get; set; }
+
+        [DisplayName("Priority Name")]
+        public string Name { get; set; }
+    }
+}
