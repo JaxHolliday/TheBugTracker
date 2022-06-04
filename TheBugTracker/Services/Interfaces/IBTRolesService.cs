05311af0f87943f9a4d5ad4eff4a89c8633e0257
Jackson Holliday <jaxholliday@gmail.com>
2022-06-04 18:28:24 -0400

    Role SService Part 1

---
+++ TheBugTracker/Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        //returns list of strings
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        //Users only for a particular company 
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+    }
+}
