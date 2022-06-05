e0d7c359c850963103ca2990305fd1a137e7f6e9
Jackson Holliday <jaxholliday@gmail.com>
2022-06-05 18:44:54 -0400

    service registered

--- TheBugTracker/Startup.cs
+++ TheBugTracker/Startup.cs
@@ -13,6 +13,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using TheBugTracker.Data;
 using TheBugTracker.Models;
+using TheBugTracker.Services;
+using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker
 {
@@ -38,6 +40,11 @@ namespace TheBugTracker
                 .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultUI()
             .AddDefaultTokenProviders();
+
+            //Registering new services 
+            services.AddScoped<IBTRolesService, BTRolesService>();
+
+
 
             services.AddControllersWithViews();
         }
