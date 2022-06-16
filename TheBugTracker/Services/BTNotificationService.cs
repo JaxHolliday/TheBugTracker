c8ba3d4f96daf98646de0ed275719cdde49461a3
Jackson Holliday <jaxholliday@gmail.com>
2022-06-16 18:54:01 -0400

    added Notification Service

---
+++ TheBugTracker/Services/BTNotificationService.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTNotificationService : IBTNotificationService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly IBTRolesService _rolesService;
+
+        public BTNotificationService(ApplicationDbContext context, IEmailSender emailSender, IBTRolesService rolesService)
+        {
+            _context = context;
+            _emailSender = emailSender;
+            _rolesService = rolesService;
+        }
+
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            try
+            {
+                await _context.AddAsync(notification);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Recipient ID
+        public async Task<List<Notification>> GetReceivedNotificationsAsync(string userId)
+        {
+            try
+            {
+                List<Notification> notifications = await _context.Notifications
+                                                                 .Include(n => n.Recipient)
+                                                                 .Include(n => n.Sender)
+                                                                 .Include(n => n.Ticket)
+                                                                     .ThenInclude(t => t.Project)
+                                                                 .Where(n => n.RecipientId == userId).ToListAsync();
+
+                return notifications;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Sender ID
+        public async Task<List<Notification>> GetSentNotificationsAsync(string userId)
+        {
+            try
+            {
+                List<Notification> notifications = await _context.Notifications
+                                                                 .Include(n => n.Recipient)
+                                                                 .Include(n => n.Sender)
+                                                                 .Include(n => n.Ticket)
+                                                                     .ThenInclude(t => t.Project)
+                                                                 .Where(n => n.SenderId == userId).ToListAsync();
+
+                return notifications;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<bool> SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            BTUser btUser = await _context.Users.FirstOrDefaultAsync(u => u.Id == notification.RecipientId);
+
+            if (btUser != null)
+            {
+                string btUserEmail = btUser.Email;
+                string message = notification.Message;
+
+                //Send Email
+                try
+                {
+                    await _emailSender.SendEmailAsync(btUserEmail, emailSubject, message);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        {
+            try
+            {
+                List<BTUser> members = await _rolesService.GetUsersInRoleAsync(role, companyId);
+
+                //class then interating variable
+                foreach (BTUser bTUser in members)
+                {
+                    notification.RecipientId = bTUser.Id;
+                    await SendEmailNotificationAsync(notification, notification.Title);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
+        {
+            try
+            {
+                //class then interating variable
+                foreach (BTUser bTUser in members)
+                {
+                    notification.RecipientId = bTUser.Id;
+                    await SendEmailNotificationAsync(notification, notification.Title);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- TheBugTracker/Startup.cs
+++ TheBugTracker/Startup.cs
@@ -48,6 +48,7 @@ namespace TheBugTracker
             services.AddScoped<IBTProjectService, BTProjectService>();
             services.AddScoped<IBTTicketService, BTTicketService>();
             services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
+            services.AddScoped<IBTNotificationService, BTNotificationService>();
 
             //Email Service
             services.AddScoped<IEmailSender, BTEmailService>();
