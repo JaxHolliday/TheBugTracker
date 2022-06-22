b344f7c401598e4b7c6b99623a8b83be44a7a3b9
Jackson Holliday <jaxholliday@gmail.com>
2022-06-22 10:51:56 -0400

    added userclaims

---
+++ TheBugTracker/Extensions/IdentityExtenions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Extensions
+{
+    public static class IdentityExtenions
+    {
+        //1 instance with "this" keyword
+        //extension method utiliazing IIdentity
+        public static int? GetCompanyId(this IIdentity identity)
+        {
+            Claim claim = ((ClaimsIdentity)identity).FindFirst("CompanyId");
+            //Ternary operator (if/else)
+            return (claim != null) ? int.Parse(claim.Value) : null;
+
+
+            //int result;
+            //if (claim != null)
+            //{
+            //    result = int.Parse(claim.Value);
+            //}
+            //else
+            //{
+            //    result = 0;
+            //}
+            //return result;
+        }
+    }
+}
