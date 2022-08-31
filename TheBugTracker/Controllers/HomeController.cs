05f2d84a29c36b2e1b7214017ed865bd67c340ad
Jackson Holliday <jaxholliday@gmail.com>
2022-08-31 19:54:25 -0400

    Dashboard GET/VM

--- TheBugTracker/Controllers/HomeController.cs
+++ TheBugTracker/Controllers/HomeController.cs
@@ -5,22 +5,45 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Extensions;
 using TheBugTracker.Models;
+using TheBugTracker.Models.ViewModels;
+using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyInfoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+                              IBTCompanyInfoService companyInfoService)
         {
             _logger = logger;
+            _companyInfoService = companyInfoService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            //instantiate dash vm
+            DashboardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+            //giving me ability to work with code in the memory and manipulate it thru LINQ | Projects that ARENT archived
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            //Selectmany to select the collections vs less
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t => t.Archived == false).ToList();
+            model.Members = model.Company.Members.ToList();
+
+
+            return View(model);
         }
 
         public IActionResult Privacy()
